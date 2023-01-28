ea0e3a52870dc06d17094b99c4bd4921474ead42
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-01-28 13:40:23 +0530

    Created Particles Background

---
+++ src/components/ParticlesBackground.js
@@ -0,0 +1,61 @@
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import { useCallback, useMemo } from "react";
+
+const ParticlesBackground = (props) => {
+  const options = useMemo(() => {
+    return {
+      background: {
+        color: "rgba(rgba(198, 47, 111, 1))",
+      },
+      fullScreen: {
+        enable: true,
+        zIndex: -1,
+      },
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 10,
+          },
+          repulse: {
+            distance: 100,
+          },
+        },
+      },
+      particles: {
+        links: {
+          enable: true,
+          distance: 130,
+        },
+        move: {
+          enable: true,
+          speed: { min: 1, max: 5 },
+        },
+        opacity: {
+          value: { min: 0.3, max: 0.7 },
+        },
+        size: {
+          value: { min: 1, max: 2.5 },
+        },
+      },
+    };
+  }, []);
+
+  const particlesInit = useCallback((engine) => {
+    loadSlim(engine);
+  }, []);
+
+  return <Particles id={props.id} init={particlesInit} options={options} />;
+};
+
+export default ParticlesBackground;
