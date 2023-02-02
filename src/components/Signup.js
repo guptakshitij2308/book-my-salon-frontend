e57af1df71c9609accad73c1b51b439f4fe3698c
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-02-02 23:28:55 +0530

    Corrected Sign in and Sign up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -10,33 +10,39 @@ const Signup = (props) => {
   };
   return (
     <div className="parent">
-    <div className="signup">
-      <h1>SignUp</h1>
+      <div className="signup">
+        <h1>Sign Up</h1>
 
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <label for="name">Name</label>
-        <input type="text" placeholder="name" id="name" name="name" />
-        <label for="email">Email</label>
-        <input
-          type="email"
-          placeholder="youremail@gmail.com"
-          id="email"
-          name="email"
-        ></input>
-        <label for="password">Password</label>
-        <input
-          type="password"
-          placeholder="********"
-          id="password"
-          name="password"
-        ></input>
-        <button  className="submit" type="submit">Sign Up</button>
-      </form>
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <label for="name">Name</label>
+          <input type="text" placeholder="name" id="name" name="name" />
+          <label for="email">Email</label>
+          <input
+            type="email"
+            placeholder="youremail@gmail.com"
+            id="email"
+            name="email"
+          ></input>
+          <label for="password">Password</label>
+          <input
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+          ></input>
+          <button className="submit" type="submit">
+            Sign Up
+          </button>
+        </form>
 
-      <button className="link-btn" onClick={() => props.onFormSwitch("login")}>
-        Already registered?<br/> Login here
-      </button>
-    </div>
+        <button
+          className="link-btn"
+          onClick={() => props.onFormSwitch("login")}
+        >
+          Already registered?
+          <br /> Login here
+        </button>
+      </div>
     </div>
   );
 };
